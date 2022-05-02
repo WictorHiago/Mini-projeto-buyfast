ac45127f0e6148be5114184a76dec0a7e521bab3
WictorHiago <wictor.hsc@gmail.com>
2022-05-02 00:13:38 -0300

    delete arquivos para reestruturação

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-const express = require('express')
-const exphbs = require('express-handlebars')//handlebars
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)//para salvar as sessoes na pasta /sessions
+// const express = require('express')//handlebars
+// const exphbs = require('express-handlebars')
+import express from 'express';
+import { engine } from 'express-handlebars';
+//const session = require('express-session')
+//const FileStore = require('session-file-store')(session)//para salvar as sessoes na pasta /sessions
+//const flash = require('express-flash')//resposavel pelas nossas mensagens
+
+const app = express();
+
+//template engine
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.listen(3000,()=>{
+    console.log('Servidor rodando em http://localhost:3000')
+})
--- database/conn.js
+++
@@ -1,6 +0,0 @@
-const { Sequilize } = require('sequelize')
-
-const sequilize = new Sequilize('cadastrodb', 'root','' {
-    host: 'localhost',
-    dialect: 'mysql',
-})
